1e395fc04bfaadedf798d9a424921e9c74502e7e
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-10-25 23:56:38 +0530

    simple section color

--- Color-Flipper/setup/app.js
+++ Color-Flipper/setup/app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+button.addEventListener('click', function(e){
+
+    const number = 3;
+    document.body.style.backgroundColor = colors[number];
+    color.textContent = colors[number];
+
+});
+
