00672b29012216db4b492f16ee295eefe1b8b809
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-10-28 23:54:28 +0530

    numbering increase, descrease, and reset

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -5,5 +5,24 @@ const value = document.querySelector("#value");
 
 btns.forEach(function (btn){
     btns.document.addEventListener("click", function (e){
-       
+        const style = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+          } else if (styles.contains("increase")) {
+            count++;
+          } else {
+            count = 0;
+          }
+      
+          if (count > 0) {
+            value.style.color = "green";
+          }
+          if (count < 0) {
+            value.style.color = "red";
+          }
+          if (count === 0) {
+            value.style.color = "#222";
+          }
+          value.textContent = count;
+        });
 });
