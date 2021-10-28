3de1e10c0223aea7b3bfa6543a6d82ff7fc2acff
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-10-28 23:53:48 +0530

    btn clicks

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+const btns = document.querySelector(".btn");
+const value = document.querySelector("#value");
+
+btns.forEach(function (btn){
+    btns.document.addEventListener("click", function (e){
+       
+});
