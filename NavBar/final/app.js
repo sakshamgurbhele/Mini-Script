355886894ff51890b36bec0e205ddf7e1dbc99e8
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-10-29 23:58:29 +0530

    clicks and drop down of table

--- NavBar/final/app.js
+++ NavBar/final/app.js
@@ -4,17 +4,30 @@
 // remove - remove class
 // toggle - toggles class
 
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
+// const navToggle = document.querySelector(".nav-toggle");
+// const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function () {
-  // console.log(links.classList);
-  // console.log(links.classList.contains("random"));
-  // console.log(links.classList.contains("links"));
-  // if (links.classList.contains("show-links")) {
-  //   links.classList.remove("show-links");
-  // } else {
-  //   links.classList.add("show-links");
-  // }
-  links.classList.toggle("show-links");
+// navToggle.addEventListener("click", function () {
+//   // console.log(links.classList);
+//   // console.log(links.classList.contains("random"));
+//   // console.log(links.classList.contains("links"));
+//   // if (links.classList.contains("show-links")) {
+//   //   links.classList.remove("show-links");
+//   // } else {
+//   //   links.classList.add("show-links");
+//   // }
+//   links.classList.toggle("show-links");
+// });
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector('.links');
+
+navToggle.addEventListener("click", function(){
+    
+    if(links.classList.contains("show-links")){
+        links.classList.remove("show-links");
+    } else {
+        links.classList.add("show-links");
+    }
+
 });
