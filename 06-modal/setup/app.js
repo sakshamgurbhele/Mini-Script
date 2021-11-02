48bf7a65d060b8b8f9e85342883e7a178da335d1
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-02 23:39:24 +0530

    alignments

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -6,10 +6,10 @@ const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
-modalBtn.addEventListener("click", function () {
-    modal.classList.add("open-modal");
-  });
+    modalBtn.addEventListener("click", function () {
+        modal.classList.add("open-modal");
+    });
 
-closeBtn.addEventListener("click", function () {
-modal.classList.remove("open-modal");
-});
+    closeBtn.addEventListener("click", function () {
+    modal.classList.remove("open-modal");
+    });
