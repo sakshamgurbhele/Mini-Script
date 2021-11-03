b22464f731d433e8cd7a46e50728bc94269e1eb8
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-03 23:38:00 +0530

    for each func

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,3 +1,10 @@
 //using selectors inside the element
 // traversing the dom
 const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+    const btn = question.querySelector(".question-btn");
+    // console.log(btn);
+  
+
+  });
