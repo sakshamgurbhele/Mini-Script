ae4ecf6170573e66a6239c73e5b7c16d8dfbd889
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-07 23:10:14 +0530

    return class menu

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -83,5 +83,16 @@ window.addEventListener("DOMContentLoaded", function () {
 function diplayMenuItems(menuItems) {
   let displayMenu = menuItems.map(function (item) {
     // console.log(item);
-    
-  });
+    return `<article class="menu-item">
+    <img src=${item.img} alt=${item.title} class="photo" />
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  }); 
