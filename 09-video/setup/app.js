0bb476e25e0a9dbf41fa76c7dbb9ebacb9f86303
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-11 23:56:07 +0530

    btn event listener

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -3,3 +3,13 @@
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", function () {
+    if (!btn.classList.contains("slide")) {
+      btn.classList.add("slide");
+      video.pause();
+    } else {
+      btn.classList.remove("slide");
+      video.play();
+    }
+  });
