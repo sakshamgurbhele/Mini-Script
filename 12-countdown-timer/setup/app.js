eb26bec7c0f86b9d08495faa1ac0475e1a8b74e6
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-16 00:12:20 +0530

    future dates

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -31,4 +31,13 @@ let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 // months are ZERO index based;
 const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
 
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
