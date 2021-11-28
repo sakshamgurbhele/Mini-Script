ea0f1fb3da3534408c183169944c97f35291c9f9
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-28 23:27:40 +0530

    intialised title and btncontainer and defined index& pages

--- 18-pagination/setup/app.js
+++ 18-pagination/setup/app.js
@@ -2,3 +2,8 @@ import fetchFollowers from './fetchFollowers.js'
 import displayFollowers from './displayFollowers.js'
 import paginate from './paginate.js'
 import displayButtons from './displayButtons.js'
+const title = document.querySelector('.section-title h1')
+const btnContainer = document.querySelector('.btn-container')
+
+let index = 0
+let pages = []
