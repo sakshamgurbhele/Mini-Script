4bf936f96dc9e2cb2ce741d028e130c710109bd3
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-11-30 09:53:47 +0530

    length of 1 & 2 of slides

--- 16-ES6-slider/setup/app.js
+++ 16-ES6-slider/setup/app.js
@@ -2,3 +2,13 @@ import data from './data.js'
 const container = document.querySelector('.slide-container')
 const nextBtn = document.querySelector('.next-btn')
 const prevBtn = document.querySelector('.prev-btn')
+// if length is 1 hide buttons
+if (data.length === 1) {
+    nextBtn.style.display = 'none'
+    prevBtn.style.display = 'none'
+  }
+  // if length is 2, add copies of slides
+  let people = [...data]
+  if (data.length === 2) {
+    people = [...data, ...data]
+  }
